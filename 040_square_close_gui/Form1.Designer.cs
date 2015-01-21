0646c21ee1f6d10b68d53262f315e5412d8a1787
sebastienadam <sebastien.adam.webdev@gmail.com>
2015-01-21 21:39:48 +0100

    Exercices GUI 20140121 (suite)

---
+++ 040_square_close_gui/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _040_square_close_gui {
+  partial class Form1 {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent() {
+      this.textBox = new System.Windows.Forms.TextBox();
+      this.buttonClose = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // textBox
+      // 
+      this.textBox.Location = new System.Drawing.Point(12, 12);
+      this.textBox.Multiline = true;
+      this.textBox.Name = "textBox";
+      this.textBox.Size = new System.Drawing.Size(483, 166);
+      this.textBox.TabIndex = 0;
+      // 
+      // buttonClose
+      // 
+      this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.buttonClose.Location = new System.Drawing.Point(216, 187);
+      this.buttonClose.Name = "buttonClose";
+      this.buttonClose.Size = new System.Drawing.Size(75, 23);
+      this.buttonClose.TabIndex = 1;
+      this.buttonClose.Text = "OK";
+      this.buttonClose.UseVisualStyleBackColor = true;
+      this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+      // 
+      // Form1
+      // 
+      this.AcceptButton = this.buttonClose;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(507, 218);
+      this.Controls.Add(this.buttonClose);
+      this.Controls.Add(this.textBox);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox;
+    private System.Windows.Forms.Button buttonClose;
+  }
+}
+
---
+++ 040_square_close_gui/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _040_square_close_gui {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+      textBox.AppendText("1");
+      for (int i = 2; i <= 20; i++) {
+        textBox.AppendText(" - "+(i * i).ToString());
+      }
+    }
+
+    private void buttonClose_Click(object sender, EventArgs e) {
+      Close();
+    }
+
+    private void Form1_Load(object sender, EventArgs e) {
+      MessageBox.Show("Chargement de la forme");
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+      DialogResult _res = MessageBox.Show("Êtes-vous sûr de vouloir quitter l'application ?", "Confirmation fermeture", MessageBoxButtons.YesNo);
+      if (_res == DialogResult.Yes) {
+        e.Cancel = false;
+      } else {
+        e.Cancel = true;
+      }
+    }
+
+    private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+      MessageBox.Show("Fin de l'application à " + DateTime.Now);
+    }
+  }
+}
