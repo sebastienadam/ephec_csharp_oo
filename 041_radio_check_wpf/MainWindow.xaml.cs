12c9bc22a8adc16720e48d36c3bc6581aa0af15e
sebastienadam <sebastien.adam.webdev@gmail.com>
2015-01-23 23:05:51 +0100

    Ex 039 en WPF
    Exercice 039 avec les RadioButton et les CheckBox refait avec Windows
    Presentation Foundation plutôt que Windows Forms.

---
+++ 041_radio_check_wpf/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls.Primitives;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _041_radio_check_wpf {
+  /// <summary>
+  /// Logique d'interaction pour MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+
+    private void setChoice() {
+      string menu = "";
+      if (groupBoxRadio != null) {
+        foreach (RadioButton item in groupBoxRadio.Children) {
+          if (item.IsChecked == true) {
+            menu = item.Content.ToString();
+            break;
+          }
+        }
+      }
+      if (groupBoxCheck != null) {
+        foreach (CheckBox item in groupBoxCheck.Children) {
+          if (item.IsChecked == true) {
+            menu += ", " + item.Content.ToString();
+          }
+        }
+      }
+      textBoxChoice.Text = menu;
+    }
+
+    private void control_CheckedChanged(object sender, RoutedEventArgs e) {
+      ToggleButton toggleButtonSender = (ToggleButton)sender;
+      string log = toggleButtonSender.Content.ToString();
+      if (toggleButtonSender.IsChecked == true) {
+        log += " sélectionné";
+      } else {
+        log += " désélectionné";
+      }
+      textBoxLog.AppendText(log + Environment.NewLine);
+      setChoice();
+    }
+  }
+}
